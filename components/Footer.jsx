6d0bfc7e01c5c934fb279a299e8afd3b826cd927
Manish Jha <manishjhazyx@gmail.com>
2022-07-14 03:41:19 +0530

    feat: added search filters

---
+++ components/Footer.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box
+      textAlign="center"
+      p="5"
+      color="gray.600"
+      borderTop="1px"
+      borderColor="gray.100"
+      position="relative"
+      w="full"
+      bottom={0}
+    >
+      2022 Estatery Inc.
+    </Box>
+  );
+};
+
+export default Footer;
