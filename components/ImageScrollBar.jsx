8f42ccebe1f1d9c2dbebef309c5e7c82c1e7fbaa
Manish Jha <manishjhazyx@gmail.com>
2022-07-15 03:10:14 +0530

    feat: property page and imagescrollbar

---
+++ components/ImageScrollBar.jsx
@@ -0,0 +1,60 @@
+import { useContext } from "react";
+import Image from "next/image";
+import { Box, Icon, Flex } from "@chakra-ui/react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Flex justifyContent="center" alignItems="center" marginRight="1">
+      <Icon
+        as={FaArrowAltCircleLeft}
+        onClick={() => scrollPrev()}
+        fontSize="2xl"
+        cursor="pointer"
+      ></Icon>
+    </Flex>
+  );
+};
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Flex justifyContent="center" alignItems="center" marginLeft="1">
+      <Icon
+        as={FaArrowAltCircleRight}
+        onClick={() => scrollNext()}
+        fontSize="2xl"
+        cursor="pointer"
+      ></Icon>
+    </Flex>
+  );
+};
+
+const ImageScrollBar = ({ data }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((image) => (
+      <Box
+        width="910px"
+        key={image.id}
+        itemID={image.id}
+        overflow="hidden"
+        p="1"
+      >
+        <Image
+          placeholder="blur"
+          blurDataURL={image.url}
+          src={image.url}
+          width={1000}
+          height={500}
+          alt="property"
+          max-width="(max-width:500px) 100px, (max-width):1023px 400px, 100px"
+        />
+      </Box>
+    ))}
+  </ScrollMenu>
+);
+
+export default ImageScrollBar;
